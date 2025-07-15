eefa3d56e9a38ff3a9cfef7772103842f125b115
Siddharth Saran <siddharthsaran@MacBook-Pro-254.local>
2025-07-15 14:22:22 -0700

    webscrape fix: filtered only necessary columns

--- webscraping/webscraping.py
+++ webscraping/webscraping.py
@@ -5,8 +5,15 @@ import requests
 from io import StringIO
 import re
 
+# columns not to rename
 NO_RENAME_COLS = ["Player", "Age", "Pos", "G", "GS", "Team"]
 
+# only have these cols as they pertain to fantasy points
+PASSING_COLS = ["Player", "Age", "Pos", "G", "GS", "Team", "P_Cmp", "P_Att", "P_Cmp%", "P_Yds", "P_Y/A","P_TD", "P_Int", "P_QBR", "P_Sk"]
+RUSHING_RECEIVING_COLS = ["Player", "Age", "Pos", "G", "GS", "Team", "Rush_Att", "Rush_Yds", "Rush_TD", "Rush_Y/A", "Rec_Tgt", "Rec_Rec", "Rec_Yds", "Rec_Y/R", "Rec_TD", "Fmb"]
+KICKING_COLS = ["Player", "Age", "Pos", "G", "GS", "Team", "0-19_FGA",	"0-19_FGM",	"20-29_FGA", "20-29_FGM", "30-39_FGA", "30-39_FGM", "40-49_FGA", "40-49_FGM", "50+_FGA", "50+_FGM",	"FGA", "FGM", "XPA", "XPM"]
+
+# Get overall page
 url = "https://www.pro-football-reference.com/years/2024/"
 data = requests.get(url)
 soup = BeautifulSoup(data.text, "html.parser")
@@ -43,6 +50,7 @@ for team_url in team_urls:
     df = df.drop(columns=["QBrec", "Rk", "Awards"])
     df.columns = [f"P_{col}" if col not in NO_RENAME_COLS else col for col in df.columns]
     df = df.iloc[:-1]
+    df = df[PASSING_COLS]
     player_list.append(df)
 
     # Rushing & Recieving
@@ -57,13 +65,16 @@ for team_url in team_urls:
 
     df["Team"] = team
     df = df.iloc[:-1]
+    df = df[RUSHING_RECEIVING_COLS]
     player_list.append(df)
 
     # Kicking
+
+    # the kicking table was commented, so find the kicking html from ID
     pattern = r'<div id="all_kicking"[\s\S]*?<!--([\s\S]*?)-->'
     match = re.search(pattern, data.text)
     kicking_html = match.group(1)
-    tables = pd.read_html(StringIO(kicking_html))
+    tables = pd.read_html(kicking_html)
     df = tables[0]
     df.columns = [
         f"{col[0]}_{col[1]}" if '-' in col[0]
@@ -74,8 +85,11 @@ for team_url in team_urls:
     df = df.drop(columns=["Rk", "Awards"])
     df["Team"] = team
     df = df.iloc[:-1]
+    df = df[KICKING_COLS]
     player_list.append(df)
     print(team)
+
+    # Wait so we don't get banned from web scraping
     time.sleep(10)
 
 
@@ -93,6 +107,9 @@ df_combined = all_player_teams.groupby('Player', as_index=False).agg(
 )
 
 df_combined = df_combined.sort_values(by='Player').reset_index(drop=True)
+convert_to_int_cols = ["Age", "G", "GS"]
+for col in convert_to_int_cols:
+    df_combined[col] = df_combined[col].astype(int)
 df_combined.to_csv("player_data.csv")
 
 
